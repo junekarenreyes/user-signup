bcd9f8a7c60c87d660023fa18a9971cc1fb3c7a3
June Reyes <june.karen.reyes@gmail.com>
2017-07-01 22:04:05 -0700

    Form validation finished

--- main.py
+++ main.py
@@ -1,93 +1,88 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect
 import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
-
-app.config['DEBUG'] = True      # displays runtime errors in the browser, too
-
-# a list of movies that nobody should have to watch
-terrible_movies = [
-    "Gigli",
-    "Star Wars Episode 1: Attack of the Clones",
-    "Paul Blart: Mall Cop 2",
-    "Nine Lives",
-    "Starship Troopers"
-]
-
-def get_current_watchlist():
-    # returns user's current watchlist--hard coded for now
-    return [ "Star Wars", "Minions", "Freaky Friday", "My Favorite Martian" ]
-
-# TODO: 
-# Modify "My Watchlist" so that you eliminate the need for the "crossoff" form in edit.html. 
-# Now, next to every list item/movie listed in "My Watchlist" you should display a button that says "I Watched it!". 
-# Clicking the button will result in a confirmation message that the movie has been watched. 
-# So you'll need to add a form within the <li> tags of "My Watchlist"
-# Once this is done, delete the "crossoff" form in edit.html
-
-# TODO:
-# Make a ratings.html template which lists all movies that have been crossed off.
-# It should have a header of <h2>Movies I Have Watched</h2>
-# Add a form for rating EACH list item/movie using a <select> dropdown with the options/values
-# in this list: ["How was it?", "*", "**", "***", "****", "*****"]
-# And with a button that says "Rate It!" to submit the user's rating.
-# Give this form the action of "/rating-confirmation" and the method of post.
-
-# TODO: 
-# Add a function, movie_ratings, to handle a get request and render the template at "/ratings"
-
-# TODO:
-# Add a function, get_watched_movies, to get the list of crossed off movies. 
-# For now, create a hard-coded list with a few movie titles. 
-
-# TODO:
-# Make a rating-confirmation.html template, to be displayed when the user rates a movie 
-# they have crossed off. 
-
-# TODO: 
-# create a rate_movie function that handles a post request on /rating-confirmation and 
-# renders the `rating-confirmation` template.
-
-@app.route("/crossoff", methods=['POST'])
-def crossoff_movie():
-    crossed_off_movie = request.form['crossed-off-movie']
-
-    if crossed_off_movie not in get_current_watchlist():
-        # the user tried to cross off a movie that isn't in their list,
-        # so we redirect back to the front page and tell them what went wrong
-        error = "'{0}' is not in your Watchlist, so you can't cross it off!".format(crossed_off_movie)
-
-        # redirect to homepage, and include error as a query parameter in the URL
-        return redirect("/?error=" + error)
-
-    # if we didn't redirect by now, then all is well
-    return render_template('crossoff.html', crossed_off_movie=crossed_off_movie)
-
-@app.route("/add", methods=['POST'])
-def add_movie():
-    # look inside the request to figure out what the user typed
-    new_movie = request.form['new-movie']
-
-    # if the user typed nothing at all, redirect and tell them the error
-    if (not new_movie) or (new_movie.strip() == ""):
-        error = "Please specify the movie you want to add."
-        return redirect("/?error=" + error)
-
-    # if the user wants to add a terrible movie, redirect and tell them the error
-    if new_movie in terrible_movies:
-        error = "Trust me, you don't want to add '{0}' to your Watchlist".format(new_movie)
-        return redirect("/?error=" + error)
-
-    # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
-    new_movie_escaped = cgi.escape(new_movie, quote=True)
-
-    return render_template('add-confirmation.html', movie=new_movie)
-
+app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    encoded_error = request.args.get("error")
-    return render_template('edit.html', watchlist=get_current_watchlist(), error=encoded_error and cgi.escape(encoded_error, quote=True))
+    template = jinja_env.get_template('index.html')
+    return template.render()
+
+@app.route("/", methods=['POST'])
+def validate_signup():
+
+    username = request.form['username']
+    password = request.form['password']
+    verifypassword = request.form['verifypassword']
+    email = request.form['email']
+
+    username_error = ''
+    password_error = ''
+    verifypassword_error = ''
+    email_error = ''
+
+    if username == '':
+        username_error = 'Please enter username'
+        username = ''
+    else:
+        username = str(username)
+        if len(username) < 3 or len(username) > 31:
+            username_error = 'Invalid username, should be between 3-20 characters'
+            username = ''
+        else:
+            username = request.form['username']
+
+    if password == '':
+        password_error = 'Please enter password'
+        password = ''
+    else:
+        password = str(password)
+        if len(password) < 3 or len(password) > 31:
+            password_error = 'Invalid password, should be between 3-20 characters'
+            password = ''
+        else:
+            password = request.form['password']
+
+    if verifypassword == "" :
+        verifypassword_error = 'Please enter password.'
+        verifypassword = ''
+    
+    if not password == verifypassword:
+        verifypassword_error = verifypassword_error + 'Passwords do not match.'
+        verifypassword = ''
+        password = ''
+
+    if len(str(email)) > 0:
+        email = str(email)
+        if len(email) < 3 or len(email) > 31:
+            email_error = 'Invalid email, should be between 3-20 characters.'
+            email = ''
+        elif "." not in email:
+            email_error = 'Invalid email. Please enter again.'
+            email = ''
+        elif "@" not in email:
+            email_error = 'Invalid email. Please enter again.'
+            email = ''
+
+
+    if not username_error and not password_error and not verifypassword_error and not email_error:
+        template = jinja_env.get_template('welcome.html')
+        return template.render(username=username)
+
+    else:
+        template = jinja_env.get_template('index.html')
+        return template.render(username_error=username_error,
+            password_error=password_error,
+            verifypassword_error=verifypassword_error,
+            email_error=email_error, username=username, email=email)
+
+    template = jinja_env.get_template('todos.html')
+    return template.render(title="TODOs", tasks=tasks)
 
 app.run()
-
